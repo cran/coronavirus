4370a9861b6be60dab253075816b529694315caa
Rami Krispin <rami.krispin@gmail.com>
2021-09-19 12:30:02 +0000

    version 0.3.30

--- R/refresh_coronvirus.R
+++ R/refresh_coronvirus.R
@@ -20,21 +20,24 @@
 #' }
 #'
 refresh_coronavirus_jhu <- function(){
-
-
   df <- NULL
+  tryCatch(
+    df <- readr::read_csv(file = "https://raw.githubusercontent.com/RamiKrispin/coronavirus/master/csv/coronavirus.csv",
+                          col_types = readr::cols(date = readr::col_date(format = "%Y-%m-%d"),
+                                                  cases = readr::col_number())),
+    error = function(c) base::message(c)
+    # warning = function(c) base::message(c),
+    # message = function(c) base::message(c)
+  )
 
-  df <- readr::read_csv("https://raw.githubusercontent.com/RamiKrispin/coronavirus/master/csv/coronavirus.csv")
 
-  df <- as.data.frame(df)
-  df$province <- ifelse(is.na(df$province), "", df$province)
 
 
   if(base::is.null(df)){
     base::message("Could not refresh the coronavirus dataset, please check your connection")
   } else{
-    df$location <- ifelse(df$province== "", df$country, paste(df$province, df$country, sep = ", "))
-    df$location_type <-  ifelse(df$province== "", "country", "state")
+    df$location <- ifelse(df$province == "" | is.na(df$province), df$country, paste(df$province, df$country, sep = ", "))
+    df$location_type <-  ifelse(df$province== "" | is.na(df$province), "country", "state")
 
     # Fixes before merging in codes
     df$location <- gsub("Korea, South", "South Korea",  df$location)
@@ -43,12 +46,19 @@ refresh_coronavirus_jhu <- function(){
     df$location <- gsub("^\\, ", "",  df$location )
 
     #get code table
-    iso_3166_2_code_table <- readr::read_csv("https://github.com/RamiKrispin/coronavirus/raw/dev-covid19r/data_raw/iso_3166_2_code_table.csv")
+    iso_3166_2_code_table <- readr::read_csv("https://github.com/RamiKrispin/coronavirus/raw/dev-covid19r/data_raw/iso_3166_2_code_table.csv",
+                                             col_types = readr::cols(
+                                               location = readr::col_character(),
+                                               location_code = readr::col_character(),
+                                               location_code_type = readr::col_character()
+                                             ))
 
     # left join codes in
     df <- base::merge(df, iso_3166_2_code_table,
                       all.x = TRUE, by = "location")
 
+    #    df$location_code <- paste(df$lat, df$long, sep = ", ")
+    #    df$location_code_type <- "latitude, longitude"
 
     #data type
     df$data_type <- ifelse(df$type == "confirmed", "cases_new",
@@ -61,7 +71,7 @@ refresh_coronavirus_jhu <- function(){
     col_order <- c( "date", "location", "location_type",
                     "location_code", "location_code_type", "data_type",
                     "value", "lat", "long")
-
+    # df$date <- as.Date(df$date)
 
     return(df[,col_order])
   }
